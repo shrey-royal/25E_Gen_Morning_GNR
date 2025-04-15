ac77fd1549fd22be22232a8e1e2c0c4288f77bfa
Shrey K <srkkadia@gmail.com>
2025-04-15 11:05:18 +0530

    Intro to Java

---
+++ Basics/FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
+/*
+To compile the java program: javac FirstProgram.java
+To run the java program: java FirstProgram
+
+*/
