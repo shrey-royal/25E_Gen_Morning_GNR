a78a32d9184bc467ba2925e628149c7aa083136d
Shrey K <srkkadia@gmail.com>
2025-04-29 19:29:34 +0530

    Command Line Args

---
+++ vs_code/Basics/CmdLineArgs.java
@@ -0,0 +1,8 @@
+public class CmdLineArgs {
+    public static void main(String[] args) {
+        // For-each loop or Advanced For Loop
+        for (String arg : args) {
+            System.out.println("arg: " + arg);
+        }
+    }
+}
